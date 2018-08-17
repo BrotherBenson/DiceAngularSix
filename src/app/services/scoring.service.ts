d233863ec15878f75483337d88f0fdaeaa821ae2
Benson Clark <bclark@daytonfreight.com>
2018-08-17 16:51:55 -0400

    adding underscore, changes to scoring

--- src/app/services/scoring.service.ts
+++ src/app/services/scoring.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Die } from '../shared/models/die';
 import { ScoreSet } from '../shared/models/score-set';
+import { _ } from 'underscore';
 
 @Injectable({
   providedIn: 'root'
@@ -12,49 +13,60 @@ export class ScoringService {
   constructor() { }
 
   score(dice: Array<Die>): number {
-    var result = 0;
     if (!dice || dice.length < 1 ){
       throw "invalid dice";
     }
-    var faces = this.getDiceFaces(dice);
-    var numberOfDice = faces.length;
+    var numberArray = this.getDiceFaces(dice);
+    return this.scoreNumbers(numberArray);
+  }
 
+  getDiceFaces(dice: Array<Die>): Array<number> {
+    var results = [];
+
+    for (let die of dice){
+      results.push(die.face);
+    }
+
+    return results;
+  }
+
+  scoreNumbers(dice: Array<number>): number {
+    var numberOfDice = dice.length;
+    
     if (numberOfDice == 5){
       return this.checkForStraight(dice);
     }
     else if (numberOfDice == 3 || numberOfDice == 4){
-      this.checkForSets(dice);
+      return this.checkForSets(dice);
     }
     else {
-      result = this.scoreIndividually(dice);
+      return this.scoreIndividually(dice);
       // check for individuals
+    }
+  }
+
+  checkForStraight(dice: Array<number>): number {   
+    return 1000;
+  }
+
+  checkForSets(dice: Array<number>): number {
+    var result = 0;
+    for(var i = 1; i == 6; i++){
+      var count = _.countBy(dice, function(number){
+        return number == i;
+      })
+      if (count >= 3){
+        result = this.scoreSet(i);
+        var remainderArray = this.removeSet(i, dice);
+        result += this.scoreIndividually(remainderArray);
+      }
     }
     
     return result;
   }
 
-  checkForStraight(dice: Array<Die>): number { 
-    var asNumbers = this.getDiceFaces(dice);
-  
-    return 1000;
-  }
-
-  checkForSets(dice: Array<Die>): number {
-    var asNumbers = this.getDiceFaces(dice);
-
-    for(var checkValue = 1; checkValue < 7; checkValue++){
-      var count = 0;
-      for (let die of dice){
-        if (die.face === checkValue){
-          count++;
-        }
-      }
-      if (count >= 3){
-        this.scoreSet(checkValue);
-      }
-    }
-
-    return 500;
+  removeSet(number: number, inputArray: Array<number>): Array<number> {
+    return _.difference(inputArray, [number, number, number]);
   }
 
   scoreSet(checkValue: number): number {
@@ -79,30 +91,18 @@ export class ScoringService {
     }
   }
 
-  scoreIndividually(dice: Array<Die>): number {
-    var asNumbers = this.getDiceFaces(dice);
-
+  scoreIndividually(dice: Array<number>): number {
     var result = 0;
-    for (let number of asNumbers){
+    for (let number of dice){
       switch (number){
         case 1:
-          return 100;
+          result += 100;
         case 5:
-          return 50;
+          result += 50;
         default:
-          return 0;
+          break;
       }
     }
-    return 0;
-  }
-
-  getDiceFaces(dice: Array<Die>): Array<number> {
-    var results = [];
-
-    for (let die of dice){
-      results.push(die.face);
-    }
-
-    return results;
+    return result;
   }
 }
